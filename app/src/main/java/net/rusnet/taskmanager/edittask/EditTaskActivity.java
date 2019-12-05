6904df5e09c8e5bb26def6655f1a54874605e550
Roman Kamyshnikov <rkam88@gmail.com>
2019-12-05 17:29:31 +0300

    create EditTaskActivity layout

--- app/src/main/java/net/rusnet/taskmanager/edittask/EditTaskActivity.java
+++ app/src/main/java/net/rusnet/taskmanager/edittask/EditTaskActivity.java
@@ -1,9 +1,14 @@
 package net.rusnet.taskmanager.edittask;
 
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.WindowManager;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,11 +19,16 @@ import net.rusnet.taskmanager.R;
 
 public class EditTaskActivity extends AppCompatActivity implements EditTaskContract.View {
 
+    public static final String TAG = "TAG_EditTaskActivity";
+
     public static final String EXTRA_IS_TASK_NEW = "net.rusnet.taskmanager.AddTaskActivity.IsTaskNew";
 
     private boolean mIsTaskNew;
 
     private Toolbar mToolbar;
+
+    private EditText mTaskNameEditText;
+    private Spinner mTaskCategorySpinner;
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -48,8 +58,14 @@ public class EditTaskActivity extends AppCompatActivity implements EditTaskContr
         setContentView(R.layout.activity_edit_task);
 
         mIsTaskNew = getIntent().getBooleanExtra(EXTRA_IS_TASK_NEW, true);
+        //todo: if task is not new get Task ID to load it
 
         initToolbar();
+
+        //todo: init presenter
+
+        initViews(savedInstanceState);
+
 
     }
 
@@ -64,5 +80,23 @@ public class EditTaskActivity extends AppCompatActivity implements EditTaskContr
                 onBackPressed();
             }
         });
+    }
+
+    private void initViews(@Nullable Bundle savedInstanceState) {
+        mTaskNameEditText = findViewById(R.id.edit_text_task_name);
+        mTaskNameEditText.setImeOptions(EditorInfo.IME_ACTION_DONE);
+        mTaskNameEditText.setRawInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_FLAG_CAP_SENTENCES);
+
+        mTaskCategorySpinner = findViewById(R.id.spinner_task_category);
+
+        if (mIsTaskNew & savedInstanceState == null) {
+            if (mTaskNameEditText.requestFocus()) {
+                getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
+            }
+        } else {
+            //todo: load data for task
+            //update mTaskNameEditText
+            //update mTaskCategorySpinner
+        }
     }
 }
